71de6499cb0211900a91cd1f5d610e908b98e6a5
Digital Revolution <stephen.vandelinder@gmail.com>
2016-05-02 14:31:35 -0600

    Added some dev tools and consolidated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,20 @@ gulp.task('default',function(){
 	console.log('gulp task intiated'); 
 }); 
 
+// Create Gulp task to rename and minify .scss
 gulp.task('process-styles', function(){	
 	return gulp.src('src/sass/*.scss')
 	.pipe(sass({style: 'expanded'}).on('error', sass.logError))
 	.pipe(autoprefixer({browsers: ['last 2 versions']}))
-	.pipe(gulp.dest('./dest/css'))
 	.pipe(rename({suffix: '.min'}))	
 	.pipe(minifycss())
 	.pipe(gulp.dest('./dest/css'))
 }); 
 
+// Create Gulp task to rename and ultify js
 gulp.task('process-scripts', function(){
 	return gulp.src('./src/js/*.js')
 	.pipe(concat('main.js'))
-	.pipe(gulp.dest('./dest/js'))
 	.pipe(rename({suffix: '.min'}))
 	.pipe(uglify())
 	.pipe(gulp.dest('./dest/js'))
@@ -34,5 +34,4 @@ gulp.task('process-scripts', function(){
 gulp.task('watch', function(){
 	gulp.watch('src/js/*.js', ['process-scripts'])
 	gulp.watch('src/sass/*.scss', ['process-styles'])
-
-})
+});
